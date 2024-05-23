4ffbf8876144b8e4dd0690030bf8d6e739570e18
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2024-05-23 14:29:30 +0530

    fixed: pages router and issue: ethers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-
+import { ThirdwebProvider } from '@thirdweb-dev/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <ThirdwebProvider>
       <Component {...pageProps} />
     </ThirdwebProvider>
-
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- sanity/schemaTypes/creator.ts
+++ sanity/schemaTypes/creator.ts
@@ -1,24 +1,26 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: 'creator',
   title: 'Creator',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'address',
       title: 'Address',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'bio',
       title: 'Bio',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -26,14 +28,14 @@ export default {
         source: 'name',
         maxLength: 96,
       },
-    },
-    {
+    }),
+    defineField({
       name: 'image',
       title: 'Image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
+    }),
   ],
-}
+})
